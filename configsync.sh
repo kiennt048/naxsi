f516913b62b9b1abd62ee3086585425f7bbd6c2c
kiennt048 <54541345+kiennt048@users.noreply.github.com>
2023-11-15 09:24:10 +0700

    Add files via upload

---
+++ configsync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RSYNC=$(sudo rsync -aizhe ssh root@192.168.18.71:/etc/nginx/ /etc/nginx/)
+
+if [ $? -eq 0 ]; then
+        if [ -n "${RSYNC}" ]; then
+                /usr/sbin/nginx -s reload
+                echo "reloaded."
+        fi
+else
+        exit 1
+fi
