1a6ea71faf0370e6072dd25bb74d108d474972ae
kiennt048 <54541345+kiennt048@users.noreply.github.com>
2023-11-15 15:55:02 +0700

    Update configsync.sh

--- configsync.sh
+++ configsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RSYNC=$(sudo rsync -aizhe "ssh -i /home/$USER/.ssh/id_rsa" kien@192.168.18.71:/etc/nginx/ /etc/nginx/)
+RSYNC=$(sudo rsync -aizhe "ssh -i /home/kien/.ssh/id_rsa" kien@192.168.18.71:/etc/nginx/ /etc/nginx/)
 
 if [ $? -eq 0 ]; then
         if [ -n "${RSYNC}" ]; then
